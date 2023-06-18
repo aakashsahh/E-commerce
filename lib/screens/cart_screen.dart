f030c7b4899db9e4f705410a0b2d7eb3bcca3b5a
aakashsahh <aakash@academiacollege.edu.np>
2023-06-18 17:58:59 +0545

    cart screen with its functionality

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,85 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
+
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  List<CartItem> cartItems = [
+    CartItem(
+      id: 1,
+      name: 'Product 1',
+      price: 50,
+      quantity: 1,
+    ),
+    CartItem(
+      id: 2,
+      name: 'Product 2',
+      price: 200,
+      quantity: 3,
+    ),
+    CartItem(
+      id: 3,
+      name: 'Product 3',
+      price: 100,
+      quantity: 2,
+    ),
+  ];
+
+  void increaseQuantity(CartItem cartItem) {
+    setState(() {
+      cartItem.quantity++;
+    });
+  }
+
+  void decreaseQuantity(CartItem cartItem) {
+    setState(() {
+      if (cartItem.quantity > 1) {
+        cartItem.quantity--;
+      }
+    });
+  }
+
+  void removeItem(CartItem cartItem) {
+    setState(() {
+      cartItems.remove(cartItem);
+    });
+  }
+
+  double getTotalPrice() {
+    double total = 0;
+    for (var cartItem in cartItems) {
+      total += cartItem.price * cartItem.quantity;
+    }
+    return total;
+  }
+
+  void pay() {
+    // Logic for payment
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Payment Successful'),
+          content: const Text('Thank you for your purchase!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                setState(() {
+                  cartItems.clear();
+                });
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,44 +106,59 @@ class CartPage extends StatelessWidget {
           },
         ),
       ),
-      body: const SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(12.0),
-          child: Center(
-            child: Text('Your cart is empty.'),
-          ),
-        ),
+      body: ListView.builder(
+        itemCount: cartItems.length,
+        itemBuilder: (context, index) {
+          final cartItem = cartItems[index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Neumorphic(
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+                color: const Color(0xFFF3F6FD),
+                intensity: 1,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: ListTile(
+                  leading: Image.asset("assets/images/Product ${index + 1}.png"),
+                  title: Text(cartItem.name),
+                  subtitle: Text('\$${cartItem.price}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () => decreaseQuantity(cartItem),
+                        icon: const Icon(Icons.remove),
+                      ),
+                      Text('${cartItem.quantity}'),
+                      IconButton(
+                        onPressed: () => increaseQuantity(cartItem),
+                        icon: const Icon(Icons.add),
+                      ),
+                    ],
+                  ),
+                  onTap: () => removeItem(cartItem),
+                ),
+              ),
+            ),
+          );
+        },
       ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
-          padding: const EdgeInsets.all(16),
-          height: 80,
-          decoration: const BoxDecoration(color: Colors.white, boxShadow: [
-            BoxShadow(
-              color: Colors.transparent,
-              blurRadius: 4,
-              spreadRadius: 2,
-            ),
-          ]),
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              const Text(
-                'Total Price: \$0.00',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
+            children: [
+              Text(
+                'Total: \$${getTotalPrice()}',
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
               ElevatedButton(
-                onPressed: () {},
-                child: const Text(
-                  'Pay',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+                onPressed: cartItems.isEmpty ? null : pay,
+                child: const Text('Pay'),
               ),
             ],
           ),
@@ -73,4 +166,18 @@ class CartPage extends StatelessWidget {
       ),
     );
   }
+}
+
+class CartItem {
+  final int id;
+  final String name;
+  final double price;
+  int quantity;
+
+  CartItem({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.quantity,
+  });
 }
